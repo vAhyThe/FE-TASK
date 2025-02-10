5417a40b0bcb8a6a0df7080c750bf496cb8dc71f
Tomáš Váhala <vahy.the@gmail.com>
2025-02-10 20:12:58 +0100

    Home View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,50 @@
+<template>
+  <div>
+    <img 
+      alt="logo" 
+      src="../assets/logo.png" 
+      class="logo-anim"
+    />
+
+    <button 
+      class="btn-anim"
+      @click="router.push('/login')"
+    >
+      Login
+    </button>
+  </div>
+</template>
+
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 </script>
 
-<template>
-  <main>
-    
-  </main>
-</template>
+<style scoped>
+/* BUTTON */
+button {
+  background-color: #007bff;
+  color: white;
+  font-size: 16px;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 100%;
+  margin-top: 20px;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+.logo-anim {
+  animation: slideFromTop 1s ease-out forwards;
+  margin-bottom: 20px;
+}
+
+.btn-anim {
+  animation: slideFromBottom 1s ease-out forwards;
+}
+</style>
